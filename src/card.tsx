19b6eff27c50ae3406515a80441a855b375e92e8
Cesar <cesarnsingi@hotmail.com>
2024-12-01 18:12:01 +0100

    type card props

--- src/card.tsx
+++ src/card.tsx
@@ -1,2 +1,9 @@
 import React from "react";
 import { GameCard } from "./types";
+
+type CardProps = { 
+    card: GameCard;
+    isFlipped: boolean;
+    isDisabled: boolean;
+    onFlip: (gameCard: GameCard) => void;
+};
