f9b067fb50a6f08a6929cc6b4ef5d3df952d8a6d
Cesar <cesarnsingi@hotmail.com>
2024-12-01 18:48:27 +0100

    function suffle array

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { GameCard } from './types';
 
+function suffleArray<T>(array: Array<T>): Array<T> {
+  for(let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1 ));
+  }
+}
+
 function createGameCards(uniqueCards: number): GameCard[] {
   const gameCards = [] as GameCard[];
   for (let value = 0; value <= uniqueCards; value++){
