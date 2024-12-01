505327c23e6ed941cdba9acc11b3d6c1e0db563d
Cesar <cesarnsingi@hotmail.com>
2024-12-01 18:43:37 +0100

    for cond

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { GameCard } from './types';
 
 function createGameCards(uniqueCards: number): GameCard[] {
   const gameCards = [] as GameCard[];
+  for (let value = 0; value <= uniqueCards; value++){
+    gameCards.push({
+      value,
+      isMatched: false,
+      id: `${value}-1`
+    })
+  }
 }
 
 function App() {
