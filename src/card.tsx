7fb51241cc6ba2822b4ccbea3568bc9d7d3164c1
Cesar <cesarnsingi@hotmail.com>
2024-12-01 18:35:59 +0100

    button property

--- src/card.tsx
+++ src/card.tsx
@@ -14,4 +14,15 @@ export function Card({ card, isFlipped, isDisabled, onFlip }: CardProps) {
     }, [onFlip, card]);
 
     const isCardFlipped = card.isMatched || isFlipped;
+
+    return ( 
+        <button 
+        className={`flex justify-center border-purple-950 p-10 m-2 w-1/5 ${isCardFlipped ? "bg-green-200"
+            : "bg-red-200"}`}
+            disabled={isCardFlipped || isDisabled}
+            onClick={handleClick}
+        >
+            {isCardFlipped ? card.value : "?"}
+        </button>
+    )
 }
