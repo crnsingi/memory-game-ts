7930ae21cac5272034a41dec499969c39a2aedac
Cesar <cesarnsingi@hotmail.com>
2024-12-06 17:43:27 +0100

    edit eslintrc

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
